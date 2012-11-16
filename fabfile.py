312a47fb7c10a1a5a1240b3fae4c6fef4549e868
Lance Parsons <lparsons@princeton.edu>
2012-11-16 18:13:58 -0500

    Initial version of sam-stats, without tests

--- fabfile.py
+++ fabfile.py
@@ -6,8 +6,15 @@ def package():
     '''
     Pacakge htseq-count for upload to toolshed
     '''
-    version = prompt("Enter version number for package:")
+    version = prompt("Enter version number for package [test]:")
+    revision = ''
+    if version != '':
+        revision = '-r "%s"' % version
+    else:
+        version = 'test'
     local('mkdir -p package')
     local('rm -f package/ea-utils_%s.tar.gz' % version)
-    #local('tar czvf package/ea-utils_%s.tar.gz --exclude "fabfile.*" --exclude "package" --exclude ".hg" *' % version)
-    local('hg archive -t tgz -r "%s" -X "fabfile.*" -X "package" -p . "package/ea-utils_%s.tar.gz"' % (version, version))
+    if version == 'test':
+        local('tar czvf package/ea-utils_%s.tar.gz --exclude "fabfile.*" --exclude "package" --exclude ".hg" *' % version)
+    else:
+        local('hg archive -t tgz %s -X "fabfile.*" -X "package" -p . "package/ea-utils_%s.tar.gz"' % (revision, version))
